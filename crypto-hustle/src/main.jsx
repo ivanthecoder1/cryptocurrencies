2cb7ae23aaac11f65834f9662c5b9fc3c6aff083
Ivan Zhang <ivanzhang0111@gmail.com>
2024-10-17 21:42:32 -0400

    Lab 6

--- crypto-hustle/src/main.jsx
+++ crypto-hustle/src/main.jsx
@@ -6,12 +6,17 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Layout from '/routes/Layout';
 import DetailView from '/routes/DetailView';
 
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index={true} element={<App />} />
         <Route index={false} path="/coinDetails/:symbol" element={<DetailView />} />
+        <Route
+          path="*"
+          element={<NotFound />}
+        />
       </Route>
     </Routes>
   </BrowserRouter>
